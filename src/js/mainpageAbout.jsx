e61bfbe6b9ec1405b423a08be2930fe3000554ff
Patryk <your@email.com>
2018-02-04 00:10:28 +0100

    Added Component mainpage with floating text, like DNA

---
+++ src/js/mainpageAbout.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+export class SpiralText extends React.Component {
+    render() {
+        return (
+            <ul>
+                <li>
+                    All computers wait at the same speed.
+                </li>
+                <li>
+                    Hey! It compiles! Ship it!
+                </li>
+                <li>
+                    "Intel Inside" is a Government Warning requireed by Law
+                </li>
+                <li>
+                    Chuck Norris counted to infinity... twice.
+                </li>
+                <li>
+                    C is quirky, flawed, and an enormous success.
+                </li>
+                <li>
+                    Beta is Latin for still doesn't work
+                </li>
+                <li>
+                    ASCII stupid question, get a stupid ANSI!
+                </li>
+                <li>
+                    God is real, unless declared integer.
+                </li>
+                <li>
+                    Any fool can use a computer. Many do.
+                </li>
+                <li>
+                    Hate cannot drive out hate; only love can do that.
+                </li>
+                <li>
+                    Java is, in many ways, C++??
+                </li>
+                <li>
+                    It works on my machine.
+                </li>
+                <li>
+                    There's no test like production
+                </li>
+                <li>
+                    There is no place like 127.0.0.1
+                </li>
+                <li>
+                    Usenet is a Mobius strand of spaghetti.
+                </li>
+                <li>
+                    You start coding. I'll go find out what they want.
+                </li>
+            </ul>
+        )
+    }
+}

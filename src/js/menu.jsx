fe27fcf02b712d4b63c305c145a76938fd1aa9b6
Patryk <your@email.com>
2018-02-04 00:11:04 +0100

    Small fixes in menu

--- src/js/menu.jsx
+++ src/js/menu.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import {SpiralText} from "./mainpageAbout.jsx";
 
 export class Menu extends React.Component {
     render() {
         return (
             <div className="col1-2 col2">
                 <div className="menu">
-                    <h1> CONTENT </h1>
+                    <div className="menu-col1">
+                        <div><button className="about-btn"><span>ABOUT</span></button></div>
+                        <div><button className="projects-btn"><span>PROJECTS</span></button></div>
+                        <div><button className="ask-btn"><span>ASK</span></button></div>
+                    </div>
+                    <div className="menu-col2">
+                        <div className="list-wrapper">
+                        <SpiralText/>
+                        </div>
+                    </div>
                 </div>
-
             </div>
         )
     }

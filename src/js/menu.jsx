acca1b57d5101523d90577435e715b74413c02ae
Patryk <kowalus23@gmail.com>
2018-02-04 22:36:12 +0100

    Added animated background to render

--- src/js/menu.jsx
+++ src/js/menu.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import {SpiralText} from "./mainpageAbout.jsx";
+
+
 
 export class Menu extends React.Component {
+
+
     render() {
         return (
             <div className="col1-2 col2">
@@ -13,7 +16,12 @@ export class Menu extends React.Component {
                     </div>
                     <div className="menu-col2">
                         <div className="list-wrapper">
-                        <SpiralText/>
+                            <svg className="loader" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 340 340">
+                                <circle cx="170" cy="170" r="160" stroke="tomato"/>
+                                <circle cx="170" cy="170" r="135" stroke="#404041"/>
+                                <circle cx="170" cy="170" r="110" stroke="#F96246"/>
+                                <circle cx="170" cy="170" r="85" stroke="#404041"/>
+                            </svg>
                         </div>
                     </div>
                 </div>

3e788b6738fa54d8085f818078a64f88f3556f6f
Patryk <kowalus23@gmail.com>
2018-02-05 13:37:50 +0100

    Added form component

--- src/js/form.jsx
+++ src/js/form.jsx
@@ -1,1 +1,12 @@
 import React from 'react';
+
+
+export class Form extends React.Component {
+    render() {
+        return (
+            <div className="col1-2 col2">
+                <h1> Hello World - that's my form of contact </h1>
+            </div>
+        )
+    }
+}

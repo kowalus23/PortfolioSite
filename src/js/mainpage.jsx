ed47137beff700a74f8050787820a512fa789ce0
Patryk <kowalus23@gmail.com>
2018-02-05 13:37:19 +0100

    Added routes

--- src/js/mainpage.jsx
+++ src/js/mainpage.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Title} from "./title.jsx";
 import {Menu} from "./menu.jsx";
+import {Portfolio} from "./portfolio.jsx";
+import {Router, Route, Link, IndexLink, IndexRoute, hashHistory} from 'react-router';
+import {Form} from "./form.jsx";
+import {About} from "./about.jsx";
 
 
 export class MainPage extends React.Component {
@@ -9,10 +13,13 @@ export class MainPage extends React.Component {
             <div className="content-container shadow">
                 <div className="margin-container">
                     <Title/>
-                    <Menu/>
-
+                    <Router history={hashHistory}>
+                        <Route path='/' component={Menu}/>
+                        <Route path='/about' component={About}/>
+                        <Route path='/portfolio' component={Portfolio}/>
+                        <Route path='/form' component={Form}/>
+                    </Router>
                 </div>
-
             </div>
         )
     }

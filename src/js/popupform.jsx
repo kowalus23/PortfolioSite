02ed9848355a0648294bfdeb2df4550862b14a94
Patryk <kowalus23@gmail.com>
2018-02-07 18:32:07 +0100

    Added form to popupform

--- src/js/popupform.jsx
+++ src/js/popupform.jsx
@@ -10,7 +10,36 @@ export class Popup extends React.Component {
                         <button onClick={this.props.closePopup}>CLOSE</button>
                     </div>
                     <div className="popup-content">
-                        <h1><sub>form</sub>CONTENT</h1>
+                        <form>
+                            <ul className="form-style-1">
+                                <li><label>Full Name <span className="required">*</span></label><input type="text"
+                                                                                                       name="field1"
+                                                                                                       className="field-divided"
+                                                                                                       placeholder="First"/>&nbsp;
+                                    <input type="text" name="field2" className="field-divided" placeholder="Last"/></li>
+                                <li>
+                                    <label>Email <span className="required">*</span></label>
+                                    <input type="email" name="field3" className="field-long"
+                                           placeholder="example@test.pl"/>
+                                </li>
+                                <li>
+                                    <label>Subject</label>
+                                    <select name="field4" className="field-select">
+                                        <option value="Advertise">Offer / cooperation</option>
+                                        <option value="Partnership">Question</option>
+                                        <option value="General Question">Issue</option>
+                                    </select>
+                                </li>
+                                <li>
+                                    <label>Your Message <span className="required">*</span></label>
+                                    <textarea className="field5 field-long field-textarea" id="field5"
+                                              placeholder="Write text here..."/>
+                                </li>
+                                <li>
+                                    <input type="submit" value="Submit"/>
+                                </li>
+                            </ul>
+                        </form>
                     </div>
                 </div>
             </div>

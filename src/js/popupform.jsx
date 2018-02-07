6668720004c072dd4f0cad7bd826362998138b3c
Patryk <kowalus23@gmail.com>
2018-02-07 15:53:28 +0100

    Added popup info

--- src/js/popupform.jsx
+++ src/js/popupform.jsx
@@ -4,13 +4,13 @@ export class Popup extends React.Component {
     render() {
         return (
             <div className='popup'>
-                <div className='popup_inner'>
+                <div className='popup-inner'>
                     <div className="popup-title">
                         <h1>{this.props.text}</h1>
-                        <button onClick={this.props.closePopup}>close me</button>
+                        <button onClick={this.props.closePopup}>CLOSE</button>
                     </div>
                     <div className="popup-content">
-                        form
+                        <h1><sub>form</sub>CONTENT</h1>
                     </div>
                 </div>
             </div>

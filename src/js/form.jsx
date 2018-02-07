bf20e285f8138e12d9fa034b40008275f927dca1
Patryk <kowalus23@gmail.com>
2018-02-07 18:32:34 +0100

    Added form content to form component

--- src/js/form.jsx
+++ src/js/form.jsx
@@ -7,10 +7,38 @@ export class Form extends React.Component {
         return (
             <div className="form">
                 <div className="form-title">
-                    <h1> Hello World - that's my form of contact </h1>
+                    <h1>Contact<sub>form</sub></h1>
                 </div>
                 <div className="form-content">
-
+                    <form>
+                        <ul className="form-style-1">
+                            <li><label>Full Name <span className="required">*</span></label><input type="text"
+                                                                                                   name="field1"
+                                                                                                   className="field-divided"
+                                                                                                   placeholder="First"/>&nbsp;
+                                <input type="text" name="field2" className="field-divided" placeholder="Last"/></li>
+                            <li>
+                                <label>Email <span className="required">*</span></label>
+                                <input type="email" name="field3" className="field-long" placeholder="example@test.pl"/>
+                            </li>
+                            <li>
+                                <label>Subject</label>
+                                <select name="field4" className="field-select">
+                                    <option value="Advertise">Offer / cooperation</option>
+                                    <option value="Partnership">Question</option>
+                                    <option value="General Question">Issue</option>
+                                </select>
+                            </li>
+                            <li>
+                                <label>Your Message <span className="required">*</span></label>
+                                <textarea className="field5 field-long field-textarea" id="field5"
+                                          placeholder="Write text here..."/>
+                            </li>
+                            <li>
+                                <input type="submit" value="Submit"/>
+                            </li>
+                        </ul>
+                    </form>
                 </div>
                 <div className="return-btn">
                     <Link to="/">
